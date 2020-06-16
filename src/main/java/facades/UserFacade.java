9e5d9559932737772b8c51d6e1958a12a37041fc
ATThorsen <thorsen.aske@gmail.com>
2020-06-17 01:15:55 +0200

    second

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -48,11 +48,6 @@ public class UserFacade {
     public String populateDB () {
     EntityManager em = emf.createEntityManager();
     
-    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
-    // CHANGE the three passwords below, before you uncomment and execute the code below
-    // Also, either delete this file, when users are created or rename and add to .gitignore
-    // Whatever you do DO NOT COMMIT and PUSH with the real passwords
 
     User user = new User("user1", "test123");
     User admin = new User("admin1", "test123");
